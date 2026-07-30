8712e69a1524a01c294b5b28335817eb6c24ff83
refanggaauli81-code <refanggaauli81@gmail.com>
2026-07-31 04:33:15 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,40 @@
-    import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-    void main() {
-      runApp(MyApp());
-    }
+void main() {
+  runApp(MyApp());
+}
 
-    class MyApp extends StatelessWidget {
-      @override
-      Widget build(BuildContext context) {
-        return MaterialApp(
-          title: 'MT5 Bot',
-          home: Scaffold(
-            appBar: AppBar(title: Text('Bot MT5 Mobile')),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Bot MT5 Siap Jalan', style: TextStyle(fontSize: 20)),
-                  SizedBox(height: 20),
-                  ElevatedButton(onPressed: () {}, child: Text('Connect ke MT5')),
-                  SizedBox(height: 10),
-                  ElevatedButton(onPressed: () {}, child: Text('Start Bot')),
-                ],
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MT5 Bot',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bot MT5 Mobile'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Bot MT5 Siap Jalan',
+                style: TextStyle(fontSize: 20),
               ),
-            ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Connect'),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Start Bot'),
+              ),
+            ],
           ),
-        );
-      }
-    }
+        ),
+      ),
+    );
+  }
+}
